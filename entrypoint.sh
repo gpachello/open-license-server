540d60a15a1b81c5b490b6402a351f3cf043ce97
Gustavo Pachello <gpachello@users.noreply.github.com>
2025-11-27 17:18:48 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "[entrypoint] Creating directories..."
-mkdir -p /ca /bup /scp
-
-echo "[entrypoint] Fixing permissions..."
-chown -R ca:ca /ca
-chown -R ca:ca /bup
-chown -R ca:ca /scp
-
-echo "[entrypoint] Switching to user ca..."
-
 # Ejecuta tail como usuario ca
-exec su -s /bin/sh ca -c "tail -f /dev/null"
+exec /bin/sh -c "tail -f /dev/null"
