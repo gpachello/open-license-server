9fb635d66b4cbd5680ddf8673909853a3179c006
Gustavo Pachello <gpachello@users.noreply.github.com>
2025-12-06 13:54:30 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,22 +3,16 @@ set -e
 
 echo "[entrypoint] Preparando entorno..."
 
-# 1. Crear base si no existe
 if [ ! -f /db/license.db ]; then
     echo "[entrypoint] Creando base SQLite..."
     sqlite3 /db/license.db "VACUUM;"
 fi
 
-# 2. Restaurar último backup si existe
 if [ -f /bup/backup.db ]; then
     echo "[entrypoint] Restaurando backup..."
     cp /bup/backup.db /db/license.db
 fi
 
-# 3. Otras tareas de inicialización
-# ...
+echo "[entrypoint] Inicialización completa."
+exec "$@"
 
-echo "[entrypoint] Servicio iniciado. Contenedor en espera."
-
-# LAST. Mantener contenedor vivo
-exec /bin/sh -c "tail -f /dev/null"
